41884ec63823a1ddbf07055935afd517a665d953
Evgenii <zvolev@mail.ru>
2022-12-18 17:10:45 +0300

    add task2.4

--- HW2/task2.4.py
+++ HW2/task2.4.py
@@ -13,3 +13,24 @@ Position two: 22
 -> [-4, -3, -2, -1, 0, 1, 2, 3, 4]
 -> There are no values for these indexes! """
 
+num = int(input('Enter the value of N: '))
+my_list = []
+for i in range(-num, num+1):
+    my_list.append(i)
+    i+=1
+print('-> ', my_list)
+
+pos1 = int(input('Position one: '))
+pos2 = int(input('Position two: '))
+
+if pos1 >= 1 and pos2 <= num*2+1:
+    deriv = my_list[pos1-1] * my_list[pos2-1]
+    print('-> ', deriv)
+else:
+    print('-> There are no values for these indexes!')
+
+
+
+
+
+
