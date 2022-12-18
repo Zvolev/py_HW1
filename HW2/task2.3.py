406c56291f7ac2455462c8912fbf28a9e7f5a37e
Evgenii <zvolev@mail.ru>
2022-12-18 16:07:11 +0300

    add task2.3

--- HW2/task2.3.py
+++ HW2/task2.3.py
@@ -5,3 +5,17 @@ in
 out
 [2.0, 2.25, 2.37, 2.441, 2.488, 2.522]
 14.071 """
+
+num = int(input('введите целое положительное число: '))
+
+my_list = []
+sum =0
+
+for i in range(1, num+1):
+    derivative = round((1 + 1/i)**i, 3)
+    sum += derivative
+    i +=1
+    my_list.append(derivative)
+    
+print(my_list)
+print(sum)
