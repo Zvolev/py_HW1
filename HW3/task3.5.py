518f92c82f70a36f94b90758ed81645a1ad4e363
Evgenii <zvolev@mail.ru>
2022-12-26 01:19:41 +0300

    add task3.5

--- HW3/task3.5.py
+++ HW3/task3.5.py
@@ -18,3 +18,20 @@ import os
 def clear():
     return os.system('cls')
 
+
+def negafibonachi(num):
+    fib_list = [0]
+    if num >= 1:
+        fib_list.append(1)
+        fib_list.insert(0, 1)
+        if num > 1:
+            for i in range(num-1):
+                a = fib_list[-1] + fib_list[-2]
+                fib_list.append(a)
+                fib_list.insert(0, ((-1)**(i+1))*a)
+    print(" ".join(map(str, fib_list)))
+
+clear()
+num = int(input('Задайте число => '))
+print('Cписок чисел Фибоначчи/Негафибоначчи: ', end='')
+negafibonachi(num)
